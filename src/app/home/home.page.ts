dc6cc01322fc24708242d04e45801ad9f5188fdd
intotito <intotito@gmail.com>
2022-05-03 11:58:07 +0100

    Two-way binding update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage {
+  quotesKeys = Object.keys;
+  quotes = {
+    days: "Daily", 
+    weeks: "Weekly",
+    months: "Monthly",
+    years: "Yearly"
+  }; 
+  range:number = 5;
+  label = this.quotes["days"];
+  constructor() {
+    console.log(this.quotesKeys(this.quotes)[3]);
+  }
 
 }
