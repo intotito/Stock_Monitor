cf8f8035dfcf4f46aaa2a63d54d744a3453ebfd8
intotito <intotito@gmail.com>
2022-05-04 22:31:38 +0100

    Working graph

--- src/app/quote-viewer/quote-viewer.page.ts
+++ src/app/quote-viewer/quote-viewer.page.ts
@@ -15,6 +15,7 @@ export class QuoteViewerPage implements OnInit {
   index: number;
   data: any;
   dataKeys: string[];
+  cardinality: number = 100;
 
   keys: string[] = [
     "Time Series (Daily)", 
@@ -49,13 +50,25 @@ export class QuoteViewerPage implements OnInit {
     console.log(this.data);
     console.log(this.dataKeys);
     let maxValue: number = Number(this.data[this.dataKeys[0]]['4. close']);
-    for(let i = 1; i < 20 - 1; i++){
-      if(Number(this.data[this.dataKeys[i]]['4. close']) > Number(this.data[this.dataKeys[i + 1]]['4. close'])){
+    for(let i = 1; i < this.cardinality - 1; i++){
+      if(Number(this.data[this.dataKeys[i]]['4. close']) > maxValue){//Number(this.data[this.dataKeys[i - 1]]['4. close'])){
         max = i;
         maxValue = Number(this.data[this.dataKeys[i]]['4. close']); 
       }
     }
     return [max, maxValue];
+  }
+
+  getMinimumValue() : number[] {
+    let min: number = 0;
+    let minValue: number = Number(this.data[this.dataKeys[0]]['4. close']);
+    for(let i = 1; i < this.cardinality - 1; i++){
+      if(Number(this.data[this.dataKeys[i]]['4. close']) < minValue){
+        min = i;
+        minValue = Number(this.data[this.dataKeys[i]]['4. close']);
+      }
+    }
+    return [min, minValue];
   }
 
   paintCanvas(){
@@ -65,27 +78,37 @@ export class QuoteViewerPage implements OnInit {
     canvas.width = (this.platform.width());
     let height: number = canvas.height
     let width: number = canvas.width;
-    let padding: number = 10;
+    let padding: number = 20;
     var ctx = canvas.getContext("2d");
-    ctx.lineWidth = 1;
+    let H: number = height - 2 * padding;
+    let W: number = width - 2 * padding;
+    ctx.lineWidth = 0.1;
+    ctx.lineCap = 'square';
     ctx.moveTo(padding, padding);
     ctx.lineTo(padding, height - padding);
     ctx.stroke();
     ctx.lineTo(width - padding, height - padding);
     ctx.stroke();
     
-    ctx.moveTo(padding, height - padding);
+    //ctx.moveTo(padding, height - padding);
+    ctx.moveTo(0,0);
     let max: number = this.getMaximumValue()[1];
-    let tickx = (width - 2 * padding) / 20;
-      let ticky = (height - 2 * padding) / max;
+    console.log("This is the maximum " + max);
     
-    for(let i = 1; i < 20; i++){
+    ctx.lineWidth = 2.0;
+
+    let tickx: number = W / this.cardinality;
+    let ticky: number = H / max;
+    ctx.moveTo(padding + (1) * tickx, padding + (H - ticky * this.data[this.dataKeys[0]]['4. close']));
+    for(let i = 1; i < this.cardinality; i++){
       let datParts = this.dataKeys[i].split('-');
       let date = new Date(Number(datParts[0]), Number(datParts[1]) - 1, Number(datParts[2]));
-      let value = Number(this.data[this.dataKeys[i]['4. close']]);
-      ctx.moveTo(padding + (i) * tickx, padding + (height - height * ticky));
-      ctx.lineTo(padding + (i + 1) * tickx, padding + (height - height * ticky));
+      let value = this.data[this.dataKeys[i]]['4. close'];
+      console.log(value);
+    //  ctx.moveTo(padding + (i) * tickx, padding + (H - ticky * value));
+      ctx.lineTo(padding + (i + 1) * tickx, padding + (H - ticky * value));
       ctx.stroke();
+   //   break;
     }
   }
 
