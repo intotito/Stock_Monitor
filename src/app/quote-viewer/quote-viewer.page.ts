11f2f85cde423ecff8df36a25af1827912989986
intotito <intotito@gmail.com>
2022-05-05 01:40:35 +0100

    Much better graphing

--- src/app/quote-viewer/quote-viewer.page.ts
+++ src/app/quote-viewer/quote-viewer.page.ts
@@ -47,8 +47,8 @@ export class QuoteViewerPage implements OnInit {
 
   getMaximumValue() : number[]{
     let max: number = 0;
-    console.log(this.data);
-    console.log(this.dataKeys);
+   // console.log(this.data);
+   // console.log(this.dataKeys);
     let maxValue: number = Number(this.data[this.dataKeys[0]]['4. close']);
     for(let i = 1; i < this.cardinality - 1; i++){
       if(Number(this.data[this.dataKeys[i]]['4. close']) > maxValue){//Number(this.data[this.dataKeys[i - 1]]['4. close'])){
@@ -78,38 +78,72 @@ export class QuoteViewerPage implements OnInit {
     canvas.width = (this.platform.width());
     let height: number = canvas.height
     let width: number = canvas.width;
-    let padding: number = 20;
+    let padding: number = 40;
     var ctx = canvas.getContext("2d");
     let H: number = height - 2 * padding;
     let W: number = width - 2 * padding;
-    ctx.lineWidth = 0.1;
-    ctx.lineCap = 'square';
+
+    ctx.beginPath();
     ctx.moveTo(padding, padding);
     ctx.lineTo(padding, height - padding);
-    ctx.stroke();
     ctx.lineTo(width - padding, height - padding);
+    ctx.lineWidth = 0.5;
+    ctx.lineCap = 'butt';
+    ctx.strokeStyle = '#0000FF';
     ctx.stroke();
+    ctx.closePath();
     
-    //ctx.moveTo(padding, height - padding);
-    ctx.moveTo(0,0);
+ 
     let max: number = this.getMaximumValue()[1];
+    let min: number = this.getMinimumValue()[1];
+
+    let range = min - (max - min) / 8;
+    console.log("This is range " + range);
     console.log("This is the maximum " + max);
-    
-    ctx.lineWidth = 2.0;
+    console.log("This is the miniumum " + min);
+ //   ctx.lineWidth = 1;
 
     let tickx: number = W / this.cardinality;
     let ticky: number = H / max;
-    ctx.moveTo(padding + (1) * tickx, padding + (H - ticky * this.data[this.dataKeys[0]]['4. close']));
+
+   // ticky = H / (max - min);
+
+    ticky = H / (max - range);
+
+//    ctx.moveTo(padding, padding);
+    let gridX = 10;
+    let gridY = 10;
+
+    ctx.beginPath();
+    for(let i = 0; i < gridY; i++){
+      ctx.moveTo(padding + (W / gridY) * (i + 1), padding + H);
+      ctx.lineTo(padding + (W / gridY) * (i + 1), padding); 
+    }
+    for(let i = 0; i < gridX; i++){
+      ctx.moveTo(padding, padding + H - (H / gridX) * (i + 1));
+      ctx.lineTo(padding + W, padding + H - (H / gridX) * (i + 1)); 
+    }
+    ctx.strokeStyle = '#00FF00';
+    ctx.lineWidth = 0.1;
+    ctx.stroke();
+    ctx.closePath();
+
+
+    ctx.beginPath();
+    ctx.moveTo(padding + (1) * tickx, padding + (H - ticky * (this.data[this.dataKeys[0]]['4. close'] - range)));
     for(let i = 1; i < this.cardinality; i++){
       let datParts = this.dataKeys[i].split('-');
       let date = new Date(Number(datParts[0]), Number(datParts[1]) - 1, Number(datParts[2]));
       let value = this.data[this.dataKeys[i]]['4. close'];
       console.log(value);
     //  ctx.moveTo(padding + (i) * tickx, padding + (H - ticky * value));
-      ctx.lineTo(padding + (i + 1) * tickx, padding + (H - ticky * value));
-      ctx.stroke();
+      ctx.lineTo(padding + (i + 1) * tickx, padding + (H - ticky * (value - range)));
    //   break;
     }
+    ctx.strokeStyle = '#FFFF00';
+    ctx.lineWidth = 1;
+    ctx.stroke();
+    ctx.closePath();
   }
 
 }
